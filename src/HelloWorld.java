8c5864579552782b30dcae35256bf95fc5390ada
goldswimmerb <goldswimmerbj@gmail.com>
2015-03-06 21:03:48 -0500

    Added my coding abilities

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("This is about the extent of my Java knowledge.");
 	}
 
 }
