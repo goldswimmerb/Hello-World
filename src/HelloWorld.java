16130061556c314437d341646a12046ebb351635
goldswimmerb <goldswimmerbj@gmail.com>
2015-03-06 21:14:02 -0500

    Added more

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("This is about the extent of my Java knowledge.");
+		System.out.println("I am Horrible at coding");
 	}
 
 }
